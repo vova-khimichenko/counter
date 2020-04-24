8839f5728ae94a964ce0c22c2541c6860aef2a35
vovkahimik <vovkahimik@gmail.com>
2020-04-24 16:54:08 +0300

    Updates

---
+++ precache-manifest.d52034f5056ed86b42d8fa0c487b7a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afd04c74a7aa53df04854e1627c9317",
+    "url": "/vovkahimik/counter/index.html"
+  },
+  {
+    "revision": "554541163ed9f47f11a0",
+    "url": "/vovkahimik/counter/static/css/main.2f255e79.chunk.css"
+  },
+  {
+    "revision": "8da5731dfe7ef01b34ee",
+    "url": "/vovkahimik/counter/static/js/2.c63ea9d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vovkahimik/counter/static/js/2.c63ea9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554541163ed9f47f11a0",
+    "url": "/vovkahimik/counter/static/js/main.b40d3a1c.chunk.js"
+  },
+  {
+    "revision": "3fa8e908e87605ca2ea2",
+    "url": "/vovkahimik/counter/static/js/runtime-main.8454c341.js"
+  }
+]);
