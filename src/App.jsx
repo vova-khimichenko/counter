bc33319fb3a14d5741657e6335678a4b88e97371
vovkahimik <vovkahimik@gmail.com>
2020-04-24 16:17:06 +0300

    counter 45 red

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import styles from './App.module.css';
 
-const App = () => {
-    return (
-        <div className={styles.App}>
-            <div className={styles.container}>
-                <div className={styles.counter}>40</div>
-                <div className={styles.buttons}>
-                    {/*<div className={styles.button}>*/}
-                    <button className={styles.button}>
-                        inc
-                    </button>
-                    {/*</div>*/}
-                    {/*<div className={styles.button}>*/}
-                    <button className={styles.button}>
-                        reset
-                    </button>
-                    {/*</div>*/}
+class App extends React.Component {
+
+    state = {
+        countValue: 40,
+    }
+
+    upCount = () => {
+        this.setState({countValue: this.state.countValue + 1})
+    }
+
+    countReset = () => {
+        this.setState({countValue: 40})
+    }
+
+    render = () => {
+
+        let classCount, buttonDisabled
+        if (this.state.countValue === 45) {
+            classCount = styles.counter45
+            buttonDisabled = true
+        } else {
+            classCount = styles.counter
+            buttonDisabled = false
+        }
+
+        return (
+            <div className={styles.App}>
+                <div className={styles.container}>
+                    <div className={classCount}>{this.state.countValue}</div>
+                    <div className={styles.buttons}>
+                        {/*<div className={styles.button}>*/}
+                        <button className={styles.button}
+                                onClick={this.upCount}
+                                disabled={buttonDisabled}>
+                            inc
+                        </button>
+                        {/*</div>*/}
+                        {/*<div className={styles.button}>*/}
+                        <button className={styles.button}
+                                onClick={this.countReset}>
+                            reset
+                        </button>
+                        {/*</div>*/}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
